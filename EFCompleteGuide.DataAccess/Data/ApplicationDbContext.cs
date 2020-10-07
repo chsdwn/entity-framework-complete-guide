e4f5b12c0236f9f8c0200a8afaf9ee8759903335
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-07 13:11:10 +0300

    3. Smartness of Entity Framework Core

--- EFCompleteGuide.DataAccess/Data/ApplicationDbContext.cs
+++ EFCompleteGuide.DataAccess/Data/ApplicationDbContext.cs
@@ -23,6 +23,7 @@ namespace EFCompleteGuide.DataAccess.Data
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
