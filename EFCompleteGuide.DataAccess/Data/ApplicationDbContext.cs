c3b9842faff60508d3988a25bfb339ec025b57b4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-07 13:08:38 +0300

    2. Primary Key and Required

--- EFCompleteGuide.DataAccess/Data/ApplicationDbContext.cs
+++ EFCompleteGuide.DataAccess/Data/ApplicationDbContext.cs
@@ -9,14 +9,24 @@ namespace EFCompleteGuide.DataAccess.Data
             => builder.UseSqlite("Data Source=CleanCode.db");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-         => modelBuilder
+        {
+            modelBuilder
                 .Entity<BookAuthor>()
                 .HasKey(bookAuthor => new { bookAuthor.Author_Id, bookAuthor.Book_Id });
+
+            /* --- FluentBookDetails --- */
+            // Key
+            modelBuilder.Entity<FluentBookDetail>().HasKey(b => b.BookDetail_Id);
+            // Required
+            modelBuilder.Entity<FluentBookDetail>().Property(b => b.NumberOfChapters).IsRequired();
+        }
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<FluentBookDetail> FluentBookDetails { get; set; }
     }
 }
---
+++ EFCompleteGuide.DataAccess/Migrations/20201007100708_AddFluentBookDetailsTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCompleteGuide.DataAccess.Migrations
+{
+    public partial class AddFluentBookDetailsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FluentBookDetails",
+                columns: table => new
+                {
+                    BookDetail_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NumberOfChapters = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "INTEGER", nullable: false),
+                    Weight = table.Column<double>(type: "REAL", nullable: false),
+                    Book_Id = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FluentBookDetails", x => x.BookDetail_Id);
+                    table.ForeignKey(
+                        name: "FK_FluentBookDetails_Books_Book_Id",
+                        column: x => x.Book_Id,
+                        principalTable: "Books",
+                        principalColumn: "Book_Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FluentBookDetails_Book_Id",
+                table: "FluentBookDetails",
+                column: "Book_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FluentBookDetails");
+        }
+    }
+}
