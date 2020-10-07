d090162798db654919668231a33876381eedbacf
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-07 14:20:10 +0300

    1. Insert Record with Migrations and Logging

---
+++ EFCompleteGuide.DataAccess/Migrations/20201007111150_AddRawCategoryToTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCompleteGuide.DataAccess.Migrations
+{
+    public partial class AddRawCategoryToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO tbl_Categories VALUES(1, 'Crime')");
+            migrationBuilder.Sql("INSERT INTO tbl_Categories VALUES(2, 'Fantasy')");
+            migrationBuilder.Sql("INSERT INTO tbl_Categories VALUES(3, 'Mistery')");
+            migrationBuilder.Sql("INSERT INTO tbl_Categories VALUES(4, 'Thriller')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
